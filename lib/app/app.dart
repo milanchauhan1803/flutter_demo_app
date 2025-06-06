ed7d81d75b8f74fe76e4f79a4d2d02457754b21c
milanchauhan1803 <milanchauhan0123@gmail.com>
2025-06-06 19:19:19 +0530

    Initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_demo/app/screens/home_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter App Demo",
+      home: HomeView(),
+    );
+  }
+}
